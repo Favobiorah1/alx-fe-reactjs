7df382611e36d36542ea96bf7d2e48868d285116
Favobiorah1 <favobiorah3@gmail.com>
2025-09-06 07:08:33 +0100

    add counter.jx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '1rem', textAlign: 'center', backgroundColor: '#222', color: '#fff' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+  return (
+    <div style={{ textAlign: 'center', margin: '20px', padding: '10px' }}>
+      <h1 style={{ color: '#646cff' }}>Hello everyone, I am learning React at ALX!</h1>
+      <p>This is a simple JSX component.</p>
+      <p>I am learning about JSX!</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
