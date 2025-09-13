f87bd254e939214ddea87ec0740451b492fa38f8
Favobiorah1 <favobiorah3@gmail.com>
2025-09-13 16:41:25 +0100

    add App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,12 @@
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeList from './components/RecipeList';
+import React from 'react';
+import AddRecipeForm from '/components/AddRecipeForm';
+import RecipeList from'/components/RecipeList';
+
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>🍳 Recipe Store (Zustand)</h1>
+      <h1>🍳 Recipe Store with Zustand</h1>
       <AddRecipeForm />
       <RecipeList />
     </div>
