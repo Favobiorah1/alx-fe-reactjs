9ee3d647555fcf1075960af2cf1359fde99f6748
Favobiorah1 <favobiorah3@gmail.com>
2025-09-14 15:06:57 +0100

    add recipeStore.js

--- recipe-sharing-app/src/Components/recipeStore.js
+++ recipe-sharing-app/src/Components/recipeStore.js
@@ -1,22 +1,28 @@
-import { create } from 'zustand';
+// src/components/recipeStore.js
+import { create } from "zustand";
 
-export const useRecipeStore = create((set) => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
 
+  // Add a recipe
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
 
-  updateRecipe: (updatedRecipe) =>
+  // Update a recipe by id
+  updateRecipe: (id, updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
       ),
     })),
 
+  // Delete a recipe by id
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 }));
+
+export default useRecipeStore;
